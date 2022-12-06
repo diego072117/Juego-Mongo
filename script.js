cf8a258a0d231dae6bab7ace83a78c9fd7c9250c
Diego_Parra <103591603+diego072117@users.noreply.github.com>
2022-12-05 19:45:56 -0600

    juego-vercion-1.2

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 // Arreglo que contiene el orden correcto de las palabras 
 
-let orden_correcto = ['pasear', 'oveja', 'rio', 'agua', 'vecina','agradecio','pastando']
+let orden_correcto = [
+    'use',
+    'createCollection',
+    'db.',
+    '.insert',
+    '"nombreLinea":',
+    '"descripccion":',
+    'holi'
+]
 
 //palabras desordenadas para mostrar en opciones 
 
-let palabras_juego = ['agua','vecina', 'pasear', 'pastando','oveja','agradecio', 'rio']
+let palabras_juego = [
+    '.insert',
+    '"nombreLinea":',
+    'use',
+    'holi',
+    'createCollection',
+    '"descripccion":',
+    'db.'
+]
 
 //container de las opciones 
 let contenedorOpciones = document.getElementById("opciones")
@@ -18,8 +34,8 @@ let txtResultado = document.getElementById("resultado")
 let posDisponible = ["", "", "", "", "", "", ""]
 
 //funcion para agregar las opciones 
-function agregarOpciones(){
-    palabras_juego.forEach(element =>{
+function agregarOpciones() {
+    palabras_juego.forEach(element => {
         let div = document.createElement("div")
         div.className = "palabra"
         div.innerHTML = element
@@ -30,16 +46,16 @@ function agregarOpciones(){
 agregarOpciones()
 
 //agrga el metodo remove onclick a cada elemento span del texto
-function agregarEliminarAspan(){
+function agregarEliminarAspan() {
     var spans = document.getElementsByTagName("span")
-    for(let i=0; i < spans.length; i++){
+    for (let i = 0; i < spans.length; i++) {
         spans[i].setAttribute("onclick", "remove(this)")
     }
 }
 agregarEliminarAspan()
 
 //funcion queu coloca el texto de opcion elegida en el correspondiente
-function completar(palabra){
+function completar(palabra) {
     let posLibre = posDisponible.indexOf("")
     document.getElementById(posLibre).innerHTML = palabra.innerHTML
     posDisponible[posLibre] = palabra.innerHTML
@@ -47,12 +63,12 @@ function completar(palabra){
 }
 
 //funcion que elimina una palabra del texto y la pone de nuevo en las opciones 
-function remove(palabra){
+function remove(palabra) {
     //agregamos la opcion nuevamente al listado
     let div = document.createElement("div")
     div.className = "palabra"
     div.innerHTML = palabra.innerHTML
-    div.setAttribute("onclick","completar(this)")
+    div.setAttribute("onclick", "completar(this)")
     contenedorOpciones.appendChild(div)
 
     //eliminamos el texto del span
@@ -67,28 +83,28 @@ function remove(palabra){
 
 
 //Funcion que comprueba si esta correcto 
-comprobar.onclick = function(){
+comprobar.onclick = function () {
     //compruebo si todavia hay palabras disponibles 
     let posLibre = posDisponible.indexOf("")
     let totalAciertos = 0
 
-    if(posLibre==-1){
-        for( i = 0; i <orden_correcto.length;i++){
-            if(orden_correcto[i]==posDisponible[i]){
+    if (posLibre == -1) {
+        for (i = 0; i < orden_correcto.length; i++) {
+            if (orden_correcto[i] == posDisponible[i]) {
                 document.getElementById(i).style.background = "#c0ff33"
                 totalAciertos++
-            }else{
+            } else {
                 document.getElementById(i).style.background = "#fb4b4b"
             }
         }
 
-        if(totalAciertos==orden_correcto.length){
+        if (totalAciertos == orden_correcto.length) {
             txtResultado.innerHTML = "Muy bien!!"
-        }else{
+        } else {
             txtResultado.innerHTML = "Exsisten errores!!"
         }
 
-    }else{
+    } else {
         alert("completa las palabras")
     }
 }
